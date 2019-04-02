49ab6f0d40a156890e41f9b571aa1dd289d0d057
Sageer Shahzad <rudph1@gmail.com>
2019-04-02 17:32:02 +0100

    Counter Function Added

---
+++ src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+
+  //state is a special property in react component basically it is an object 
+  //that include any data that this component need
+  state = {
+    count: 0
+  };
+
+  //eventhandeler
+  handleIncrement = product => {
+    this.setState({ count: this.state.count + 1 })
+  };
+
+
+  render() {
+
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: 1 })}
+          className={"btn btn-primary m-2"}>
+          Increment
+                </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero' : count;
+  }
+}
+
+export default Counter;
