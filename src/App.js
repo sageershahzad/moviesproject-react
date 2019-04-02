b27a93c0575ce8b6380a1afd24a02448da4fac5b
Sageer Shahzad <rudph1@gmail.com>
2019-04-02 17:27:57 +0100

    movies updelfunctions

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import Movies from './components/movies';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main role="main" className="container">
+        <Movies />
+      </main >
+
+    );
+  }
+}
+
+export default App;
